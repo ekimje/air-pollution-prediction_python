8115967ba807538feec463c98dae40bf051ffa10
김지윤 <blue201232@gmail.com>
2026-03-25 14:18:55 +0900

    미세먼지, 미세먼지+기상 one-hot encoding 처리

---
+++ PM10_and_weather_processing.py
@@ -0,0 +1,32 @@
+# 미세먼지 + 기상 데이터
+import pandas as pd
+
+data = pd.read_csv('C:\\air_pollution_project_python\\weather_pm.csv',encoding='utf-8-sig')
+data['datatime'] = pd.to_datetime(data['datetime'])
+
+data = data.sort_values(['지역','datetime'].copy())
+
+columns = ['PM10','PM10','wind','temp','humidity']
+
+for col in columns:
+    data[f'{col}_t-1'] = data.groupby('지역')[col].shift(1)
+    data[f'{col}_t-2'] = data.groupby('지역')[col].shift(2)
+
+data['target_t+1'] = data.groupby('지역')['PM10'].shift(-1)
+data['target_t+3'] = data.groupby('지역')['PM10'].shift(-3)
+
+data = data.dropna()
+
+data = pd.get_dummies(data, columns=['지역']) # 지역 더미 변수화
+
+# 시계열 분리. 한... 2026년 3월 3일까지
+train = data[data['datetime']<'2026-03-03'].copy()
+test = data[data['datetime']>='2026-03-03'].copy()
+
+x_train = train.drop(['datetime','target_t+1','target_t+3'], axis=1)
+y_train_1 = train['target_t+1']
+y_train_3 = train['target_t+3']
+
+x_test = test.drop(['datetime','target_t+1','target_t+3'], axis=1)
+y_test_1 = test['target_t+1']
+y_test_3 = test['target_t+3']
--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime, timedelta
 
 # 다운 받은 파일들 excel -> csv 파일 변경
-input_files = 'C:/air_pollution_project_python/airKorea_execl'
+input_files = 'C:/air_pollution_project_python/airKorea_excel'
 
 for root, dirs, files in os.walk(input_files):
     for file in files:
@@ -43,7 +43,7 @@ for root, dirs, files in os.walk(input_files):
     
     for idx, file in enumerate(csv_file):
         file_path = os.path.join(root, file)
-        df = pd.read_csv(input)
+        df = pd.read_csv(file_path)
         
         df["측정소명"] = df["측정소명"].str.replace("[","",regex=False).str.replace("]","",regex=False)
         df["지역"] = df["측정소명"].str.split().str[0]
@@ -73,7 +73,7 @@ for root, dirs, files in os.walk(input_files):
         
 # 기상 데이터와 미세 먼지의 공통 데이터 컬럼 맞추기. 지역과 시간.
 
-df =pd.read_csv('C:/air_pollution_project_python/OBS_ASOS_TIM_20260318081505.csv', encoding='cp949')
+df =pd.read_csv('C:\\air_pollution_project_python\\OBS_ASOS_TIM_20260318081505.csv', encoding='cp949')
 
 df=df.drop('지점',axis=1)
 print(df.columns)
@@ -82,13 +82,9 @@ df = df.rename(columns={'지점명':'지역','일시':'datetime','기온(°C)':'
 pm_df['datetime'] = pd.to_datetime(pm_df['datetime'])
 df['datetime'] = pd.to_datetime(df['datetime'])
 
-
-print(pm_df)
-print(df)
-
-df.to_csv('weather_processed.csv',encoding="utf-8-sig")
+df.to_csv('C:\\air_pollution_project_python\\weather_processed.csv',encoding="utf-8-sig")
 
 # 두 데이터 merge. 특정 열을 기준으로 정렬. 지역과 datetime 일치
 
 merge_df = pd.merge(df, pm_df, on=['지역','datetime'],how='inner')
-merge_df.to_csv("weather_pm.csv")
+merge_df.to_csv("C:\\air_pollution_project_python\\weather_pm.csv",index=False, encoding="utf-8-sig")
